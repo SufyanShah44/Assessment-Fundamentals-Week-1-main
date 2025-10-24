1084a1512bfb8aca9bd0ae8f5657431dd1fdb829
Sufyan <trainee.sufyan.shah@sigmalabs.co.uk>
2025-10-24 17:29:37 +0100

    Part C incomplete

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -2,11 +2,24 @@
 Note: Do not add ANY variables to the global scope. This WILL break the tests.
 """
 
-
 def generate_invoice(receipt_string: str) -> str:
+    """Returns a VAT Receipt"""
+    lines = receipt_string.splitlines()
+    for line in lines:
+        if "Total:" in line:
+            total = float(line.split("£")[1])
 
-    return  # return the invoice string
+    vat = total * 0.2
+    total_with_vat = total + vat
 
+    invoice = "VAT RECEIPT\n"
+    invoice += "\n"
+    for line in lines:
+        invoice += line + "\n"
+    invoice +=  f"VAT: £{vat:.2f}\n"
+    invoice += f"Total inc VAT: £{total_with_vat:.2f}"
+
+    return invoice # return the invoice string
 
 if __name__ == "__main__":
     receipt_string = """Bread x 2 - £3.60
